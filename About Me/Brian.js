b78070313b88d91e79c9bb38e6339039c8b9aae6
BriHigg <pixelgp2@gmail.com>
2018-07-08 22:04:32 -0700

    git push

---
+++ About Me/Brian.js
@@ -0,0 +1,15 @@
+using System;
+namespace Demo
+{
+//A Simple application using C#
+	class Program
+	{
+	//The main function
+	static void Main(string[] args)
+		{
+		//Writing Hello World to the console
+		Console.WriteLine("Hello World! This is Brian");
+		Console.Read();
+		}
+	}
+}
